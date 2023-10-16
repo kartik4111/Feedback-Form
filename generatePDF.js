5413643c13c8ae66108f500e2b01acc5e486fb8f
kartik4111 <cmcoachpunk@gmail.com>
2023-10-16 16:13:19 +0530

    little tweaks

--- generatePDF.js
+++ generatePDF.js
@@ -2,6 +2,11 @@ async function generatePDF() {
     const averageOfAveragesJSON = localStorage.getItem("averageOfAverages");
     const averageOfAverages = JSON.parse(averageOfAveragesJSON);
 
+    if (averageOfAverages === null) {
+        alert("Generate the excel file first.");
+        return
+    }
+
     // Define key replacements
     const keyReplacements = {
         "C-I": "About The Programme",
@@ -22,7 +27,7 @@ async function generatePDF() {
     const doc = new jspdf.jsPDF()
     var year = new Date().getFullYear();
     // Add the heading at the top of the page
-    doc.setFont("Times New Roman", "bold");
+    doc.setFont("times", "bold");
     doc.setFontSize(16);
     doc.text("DEPARTMENT OF COMPUTER SCIENCE & IT", 105, 15, { align: "center" }); // Center-aligned text
     doc.text("UNIVERSITY OF JAMMU", 105, 20, { align: "center" }); // Center-aligned text
@@ -36,6 +41,14 @@ async function generatePDF() {
     for (const key in averageOfAverages) {
         rows.push([key, averageOfAverages[key]]);
     }
-    doc.autoTable({ head: [columns], body: rows, margin: { top: 40 } });
-    doc.save('table.pdf')
+    doc.autoTable({
+        head: [columns],
+        body: rows,
+        margin: { top: 50 },
+        theme: "plain",
+        headStyles: { lineWidth: 0.1, cellPadding:3,halign:"center" },
+        bodyStyles: { lineWidth: 0.1, cellPadding:7, halign:"center" },
+        columnStyles: { 0: { cellWidth: 150 }},
+    });
+    doc.save('table.pdf');
 }
